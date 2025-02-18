85bc360d4c5560219679e9fe40207d7842d423d7
Jollypru <Jollyprumarma2020@gmail.com>
2025-02-19 01:18:40 +0600

    added sort functionality

--- src/pages/AllTrips/AllTrips.jsx
+++ src/pages/AllTrips/AllTrips.jsx
@@ -1,12 +1,14 @@
 import { useQuery } from '@tanstack/react-query';
 import axios from 'axios';
-import React from 'react';
+import React, { useState } from 'react';
 import { TbCurrencyTaka } from 'react-icons/tb';
 import { NavLink } from 'react-router-dom';
 import useAxiosPublic from '../../hooks/useAxiosPublic';
 
 const AllTrips = () => {
     const axiosPublic = useAxiosPublic();
+    const [sortType, setSortType] = useState('');
+
 
     const { data: packages = [], isLoading } = useQuery({
         queryKey: ['packages'],
@@ -19,12 +21,35 @@ const AllTrips = () => {
     if (isLoading) {
         return <span className="loading loading-spinner loading-lg"></span>
     }
+
+    const sortedPackages = [...packages].sort((a, b) => {
+        if (sortType === 'price-asc') return a.price - b.price;
+        if (sortType === 'price-desc') return b.price - a.price;
+        if (sortType === 'title-asc') return a.title.localeCompare(b.title);
+        if (sortType === 'title-desc') return b.title.localeCompare(a.title);
+        return 0; // Default, no sorting
+    });
+
     return (
-        <div className='bg-base-200 min-h-screen max-w-screen-xl mx-auto pt-20 pb-10 px-3 md:px-10'>
+        <div className='bg-base-200 min-h-screen pt-20 pb-10 px-3 md:px-10'>
             <h2 className='text-4xl text-center font-bold'>All Trips</h2>
+
+            <div className='flex justify-end my-4'>
+                <select 
+                    className='border border-gray-300 p-2 rounded-sm  bg-base-200' 
+                    onChange={(e) => setSortType(e.target.value)}
+                >
+                    <option value="">Sort By</option>
+                    <option value="price-asc">Price, Low to High</option>
+                    <option value="price-desc">Price, High to Low</option>
+                    <option value="title-asc">Alphabetically, A-Z</option>
+                    <option value="title-desc">Alphabetically, Z-A</option>
+                </select>
+            </div>
+
             <div className='grid grid-cols-1 md:grid-cols-2 gap-4 mt-8'>
                 {
-                    packages.map((pkg) => (
+                    sortedPackages.map((pkg) => (
                         <div key={pkg._id} className='bg-base-100 p-3 flex flex-col md:flex-row justify-between gap-3 rounded-sm shadow-xl'>
                             <img className='w-2/5' src={pkg.coverImage} alt="" />
                             <div>
